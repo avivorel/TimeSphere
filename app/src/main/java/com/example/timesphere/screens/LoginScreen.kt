6fc292be150aca9328c6b30c3e80816755863850
OfirElya <ofirelya2@gmail.com>
2025-03-18 18:40:45 +0200

    cosmeticss to login and preauth screens

--- app/src/main/java/com/example/timesphere/screens/LoginScreen.kt
+++ app/src/main/java/com/example/timesphere/screens/LoginScreen.kt
@@ -3,10 +3,16 @@ package com.example.timesphere.screens
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -16,11 +22,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.timesphere.ui.theme.BackgroundContainer
+import com.example.timesphere.ui.theme.RoundedCornerCardTop
 import com.example.timesphere.viewmodels.AppViewModel
 
 @Composable
@@ -29,38 +39,91 @@ fun LoginScreen(navHost: NavHostController, appViewModel: AppViewModel = viewMod
     var password by remember { mutableStateOf("") }
     val context = LocalContext.current
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        TextField(keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),value = email, onValueChange = { email = it }, label = { Text("Email") })
-        TextField(value = password, onValueChange = { password = it }, label = { Text("Password") })
-
-        Button(
-            onClick = {
-                val trimmed_email = email.trim()
-                appViewModel.signIn(trimmed_email, password) { isSuccess ->
-                    if (isSuccess) {
-                        appViewModel.fetchUser()
-                        navHost.navigate("home_screen") {
-                            popUpTo("login_screen") { inclusive = true }
-                        }
-                    } else {
-                        val errorMessage = when (isSuccess) {
-                            true-> "login login"
-//                            is FirebaseAuthException -> isSuccess.message ?: "Login failed"
-                            else -> "Login failed"
-
-                        }
-                        Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            },
-            modifier = Modifier.padding(16.dp)
+    BackgroundContainer {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Login")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .padding(top = 32.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Welcome to", style = MaterialTheme.typography.headlineSmall, color = MaterialTheme.colorScheme.onBackground, textAlign = TextAlign.Center)
+                    Text("TimeSphere", style = MaterialTheme.typography.headlineLarge, color = MaterialTheme.colorScheme.primary, textAlign = TextAlign.Center)
+                }
+            }
+
+            RoundedCornerCardTop(
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(32.dp),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            "Login",
+                            style = MaterialTheme.typography.headlineMedium,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier.padding(vertical = 24.dp)
+                        )
+
+                        TextField(
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                            value = email,
+                            onValueChange = { email = it },
+                            label = { Text("Email") },
+                            placeholder = { if (email.isEmpty()) Text("Enter Email") },
+                            modifier = Modifier.fillMaxWidth().onFocusChanged { if (it.isFocused) email = "" },
+                            singleLine = true
+                        )
+
+                        Spacer(modifier = Modifier.padding(8.dp))
+
+                        TextField(
+                            value = password,
+                            onValueChange = { password = it },
+                            label = { Text("Password") },
+                            placeholder = { if (password.isEmpty()) Text("Enter Password") },
+                            modifier = Modifier.fillMaxWidth().onFocusChanged { if (it.isFocused) password = "" },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                            singleLine = true
+                        )
+
+                        Spacer(modifier = Modifier.padding(8.dp))
+
+                        Button(
+                            onClick = {
+                                val trimmedEmail = email.trim()
+                                appViewModel.signIn(trimmedEmail, password) { isSuccess ->
+                                    if (isSuccess) {
+                                        appViewModel.fetchUser()
+                                        navHost.navigate("home_screen") {
+                                            popUpTo("login_screen") { inclusive = true }
+                                        }
+                                    } else {
+                                        val errorMessage = "Login failed"
+                                        Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+                                    }
+                                }
+                            },
+                            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+                        ) {
+                            Text("Continue")
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                onClick = {}
+            )
         }
     }
 }
--- app/src/main/java/com/example/timesphere/screens/PreAuthScreen.kt
+++ app/src/main/java/com/example/timesphere/screens/PreAuthScreen.kt
@@ -2,36 +2,76 @@ package com.example.timesphere.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.timesphere.navhost.OnBoardingDestinations
+import com.example.timesphere.ui.theme.BackgroundContainer
+import com.example.timesphere.ui.theme.RoundedCornerCardTop
 
 @Composable
 fun PreAuthScreen(navHost: NavHostController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(
-            onClick = { navHost.navigate("login_screen") }, // Navigate to LoginScreen
-            modifier = Modifier.padding(16.dp)
+    BackgroundContainer {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Login")
-        }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .padding(top = 32.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Welcome to", style = MaterialTheme.typography.headlineSmall, color = MaterialTheme.colorScheme.onBackground, textAlign = TextAlign.Center)
+                    Text("TimeSphere", style = MaterialTheme.typography.headlineLarge, color = MaterialTheme.colorScheme.primary, textAlign = TextAlign.Center)
+                }
+            }
 
-        Button(
-            onClick = { navHost.navigate(OnBoardingDestinations.WelcomeScreen.route) }, // Navigate to WelcomeScreen
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text("Sign Up")
+            RoundedCornerCardTop(
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(32.dp),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        Button(
+                            onClick = { navHost.navigate("login_screen") },
+                            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+                        ) {
+                            Text("Login")
+                        }
+
+                        Button(
+                            onClick = { navHost.navigate(OnBoardingDestinations.WelcomeScreen.route) },
+                            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+                        ) {
+                            Text("Sign Up")
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                onClick = {}
+            )
         }
     }
 }
