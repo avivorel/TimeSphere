be440386c6987036fa3408e180a925fe92290705
OfirElya <ofirelya2@gmail.com>
2025-03-18 17:28:21 +0200

    changed verify screen for better password handling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,5 +89,7 @@ dependencies {
     implementation ("androidx.compose.animation:animation:1.6.8")
     implementation ("androidx.compose.material3:material3:1.2.1")
     implementation ("androidx.compose.ui:ui:1.6.8")
+    implementation("androidx.compose.material:material-icons-extended")
+
 
 }
